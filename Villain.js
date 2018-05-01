bd4c5f78499703d7685bc8348d1461149454ee79
carly harper <carly.harper@thingtech.com>
2018-05-01 09:54:49 -0400

    now deducting points

--- Villain.js
+++ Villain.js
@@ -4,5 +4,4 @@ class Villain extends Person {
         this.imageURL = "https://1.bp.blogspot.com/-MoaYDykD4lk/WWSLeV8xyqI/AAAAAAAABQg/qrqKsCfFq7kJXR-HBMSZ7i_AyoY4YAqyQCK4BGAYYCw/s1600/24c81-supervillainhandbook.jpg";
         this.selector = '.villain';
     }
-
 }
